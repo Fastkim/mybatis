fd50de8d631c64a6e3a27aa6ba7faa86a05de4d6
'dahunkim' <'ekgns019@gmail.com'>
2024-05-20 17:22:25 +0900

    2024-05-20 Center

--- src/ch01/Main.java
+++ src/ch01/Main.java
@@ -19,7 +19,25 @@ public class Main {
 		
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver"); // jdbc 개발자가 oracle하고 이렇게 연결하라고 정해놨다.
-			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD); // oracle로 request를 보내고, response를 받는다.
+			
+			String query = "SELECT user_id, user_name, reg_date FROM users"; // request 부분
+			stmt = conn.prepareStatement(query); // sql을 준비하고
+			rs = stmt.executeQuery(); // response를 받는다. result set은 table과 유사하다.
+			// rs의 각각의 레코드를 객체로 만드는 과정
+			
+			// OR mapping (Object Relation) -> 여기서 Object을 Pojo, 혹은 domain이라고 부른다.
+			// mybatis는 or mapping 라이브러리에 관련한 프레임워크이다
+			User user = null;
+			while(rs.next()) { // row세상 : db , object세상 : java 연결
+				// next에서 true가 return되면 row가 있고, false면 row가 없다.
+				user = new User();
+				user.setUserId(rs.getInt(1)); // getInt(1 : 첫번째 칼럼 불러오기)
+				user.setUserName(rs.getString(2)); // varchar2 타입은 getString으로 읽으면댐
+				user.setRegDate(rs.getDate(3));
+				
+				System.out.println(user);
+			}
 		} catch(Exception e) {
 			e.printStackTrace();
 		} finally {
